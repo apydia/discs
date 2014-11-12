13a6c0e40e7da5fee7d567b5e712776f85ebd450
Jan Reinsch <reinsch.jan@gmail.com>
2014-11-12 13:33:29 +0100

    #5 created magnetic actor prefab that pulls-in / repells other game objects

---
+++ Scripts/MagneticActor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagneticActor : MonoBehaviour {
+
+	public float power;
+	public float radius;
+
+	// Use this for initialization
+	void Start () {
+		power = -1000f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.attachedRigidbody && other.gameObject.tag == "Player") {
+
+			float dist = Vector3.Distance(other.gameObject.transform.position, transform.position);
+
+			Vector3 dir = other.gameObject.transform.position - transform.position;
+			other.attachedRigidbody.AddForce (dir*power/(dist*dist));
+			other.rigidbody.AddForce (dir);
+		}
+	}
+}
