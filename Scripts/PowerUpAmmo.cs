6b9c6248cd9f2e6e393384199a0a4998b4d072e6
Jan Reinsch <reinsch.jan@gmail.com>
2014-11-16 21:48:38 +0100

    #17 #23 crates are now temporarily hidden when colliding with gem

--- Scripts/PowerUpAmmo.cs
+++ Scripts/PowerUpAmmo.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Reflection;
+using System;
 
 public class PowerUpAmmo : PowerUpBase {
 
@@ -12,5 +14,45 @@ public class PowerUpAmmo : PowerUpBase {
 			other.gameObject.GetComponent<PlayerController>().gunAmmoAmount += 10;
 			other.gameObject.GetComponent<PlayerLogic>().PowerUpDestroyed(id);
 		}
+		if (other.gameObject.tag == "FlagItem") {
+			this.gameObject.renderer.enabled = false;
+			//GameObject.Destroy(this.gameObject);
+		} else {
+			this.gameObject.renderer.enabled = true;
+		}
+		/*
+		if (other.gameObject.tag == "PowerUpCrate") {
+			GameObject doomed = other.gameObject;
+			Debug.Log ("GetName(): " + GetName());
+
+
+			Type t = Type.GetType ("PowerUpBase");
+			PowerUpBase b = (PowerUpBase)other.gameObject.GetComponent(t);
+			if (this.createTime > b.createTime) {
+				doomed = this.gameObject;
+			}
+			Vector3 pos = doomed.transform.position;
+			pos.y += 5f;
+			doomed.transform.position = pos;
+			//GameObject.Destroy(doomed);
+
+		}*/
+
+		/*
+		if (other.gameObject.tag == "PowerUpCrate") {
+			GameObject doomed = other.gameObject;
+			if (this.createTime > other.gameObject.GetComponent<PowerUpBase>().createTime) {
+				doomed = this.gameObject;
+			}
+			GameObject.Destroy(doomed);
+		}*/
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "FlagItem") {
+			this.gameObject.renderer.enabled = true;
+			//GameObject.Destroy(this.gameObject);
+		} 
+
 	}
 }
--- Scripts/PowerUpBase.cs
+++ Scripts/PowerUpBase.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using System.Collections;
+using System.Reflection;
+using System;
 
 public class PowerUpBase : MonoBehaviour, PowerUp {
 	
 	public int id;
 	public Texture texture;
-	
+	public double createTime;
+
 	// Use this for initialization
 	void Start () {
 		renderer.material.mainTexture = texture;
@@ -30,6 +33,7 @@ public class PowerUpBase : MonoBehaviour, PowerUp {
 	
 	public void Init(int id, object[] initData, double createTime) {
 		this.id  = id;
+		this.createTime = createTime;
 	}
 	
 	public virtual void Activate(GameObject player, Vector3 pos) {
@@ -39,5 +43,35 @@ public class PowerUpBase : MonoBehaviour, PowerUp {
 		if (other.gameObject.tag == "Player") {
 			other.gameObject.GetComponent<PlayerLogic>().PowerUpCollected(this);
 		}
+		if (other.gameObject.tag == "FlagItem") {
+			this.gameObject.renderer.enabled = false;
+			//GameObject.Destroy(this.gameObject);
+		} else {
+			//this.gameObject.renderer.enabled = true;
+		}
+		/*
+		if (other.gameObject.tag == "PowerUpCrate") {
+			GameObject doomed = other.gameObject;
+			Debug.Log ("GetName(): " + GetName());
+
+			Type t = Type.GetType ("PowerUpBase");
+			PowerUpBase b = (PowerUpBase)other.gameObject.GetComponent(t);
+			if (this.createTime > b.createTime) {
+				doomed = this.gameObject;
+			}
+			Vector3 pos = doomed.transform.position;
+			pos.y += 5f;
+			doomed.transform.position = pos;
+			//GameObject.Destroy(doomed);
+			//
+		}*/
 	}
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "FlagItem") {
+			this.gameObject.renderer.enabled = true;
+			//GameObject.Destroy(this.gameObject);
+		} 
+	
+	}
+
 }
